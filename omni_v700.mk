c9a56289fe4eeb121100ee91eed39407a625ade5
darknightmare <rubentkd98@gmail.com>
2015-05-15 18:15:21 +0200

    v700: Adapt to Omni

---
+++ omni_v700.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/lge/v700/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := v700
+PRODUCT_NAME := omni_v700
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_MODEL := LG-V700
