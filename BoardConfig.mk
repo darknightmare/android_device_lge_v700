a1934ce8cef861dcab5ff643c5b3cee89651dc5b
darknightmare <rubentkd98@gmail.com>
2015-05-19 15:47:59 +0200

    v700: Add mkbootimg and bump script

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,8 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --dt device/lge/v700/dt.img --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/v700/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG := true
 
 # USB Mounting
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
